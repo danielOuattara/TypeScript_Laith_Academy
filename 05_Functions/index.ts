5fde67781d47ab64f246f996c85c26349421b450
danielOuattara <daniel.ouattara@gmx.com>
2023-02-22 21:43:27 +0100

    logic update

--- 05_Functions/index.ts
+++ 05_Functions/index.ts
@@ -4,7 +4,7 @@ const add = (num1, num2) => {
   return num1 + num2;
 };
 
-add(true, 1);
+add(true, 1); // Accepted, but Not Logic
 
 //--------------------------------
 
@@ -12,7 +12,7 @@ const add2 = (num1: any, num2: any) => {
   return num1 + num2;
 };
 
-add2(true, 1);
+add2(true, 1); // Accepted, but Not Logic
 
 //--------------------------------
 
@@ -20,21 +20,21 @@ const add3 = (num1: number, num2: number) => {
   return num1 + num2;
 };
 
-add3(true, 1); // X Incorrect
 add3(4, 1); // Correct
+add3(true, 1); // X Incorrect
 add3([], 1); // X Incorrect
 add3({}, 1); // X Incorrect
 //--------------------------------
 
 // ADVICE: Never let TypeScript infer your function return, DO it yourself !
-const substract = (num1: number, num2: number): number => {
+const subtract = (num1: number, num2: number): number => {
   return num1 + num2;
 };
 
-substract(true, 1); // X Incorrect
-substract(4, 1); // Correct
-substract([], 1); // X Incorrect
-substract({}, 1); // X Incorrect
+subtract(true, 1); // X Incorrect
+subtract(4, 1); // Correct
+subtract([], 1); // X Incorrect
+subtract({}, 1); // X Incorrect
 
 //-------------------------------------------------------
 const logic = (num1: number, num2: number): boolean => {
